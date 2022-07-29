2563fce020cd05b7f489600e0aa9230eace4e69a
Drew Repasky <drew@repasky.org>
2022-07-29 14:48:34 -0400

    HTML form templates

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,7 +12,7 @@ import (
 type Server struct{}
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if (r.Method == http.MethodGet || r.Method == http.MethodHead) && r.URL.Path != "/" {
+	if r.Method == http.MethodGet || r.Method == http.MethodHead {
 		s.handleGET(w, r)
 		return
 	}
@@ -28,9 +29,21 @@ func (s *Server) handlePOST(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) handleGET(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		s.renderTemplate(w, r, nil, "layout", "dist/layout.html", "dist/index.html")
+		return
+	}
 	noteID := strings.TrimPrefix(r.URL.Path, "/")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("You requested the note with the ID '%s'.", noteID)))
+}
+
+func (s *Server) renderTemplate(w http.ResponseWriter, r *http.Request, data interface{}, name string, files ...string) {
+	t := template.Must(template.ParseFiles(files...))
+	err := t.ExecuteTemplate(w, name, data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
