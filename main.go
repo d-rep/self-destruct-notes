62571cd216561394a07119ada88ebf181f4f047c
Drew Repasky <drew@repasky.org>
2022-07-29 14:35:11 -0400

    added routes

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func webServer(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello, world!"))
+type Server struct{}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet || r.Method == http.MethodHead {
+		noteID := strings.TrimPrefix(r.URL.Path, "/")
+		if noteID != "" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("You requested the note with ID %q", noteID)))
+			return
+		}
+	}
+
+	if r.Method == http.MethodPost && r.URL.Path == "/" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("You posted to /"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("Not Found"))
 }
 
 func main() {
@@ -18,7 +37,7 @@ func main() {
 	}
 	addr := "localhost:" + port
 	log.Printf("Starting web server, listening on %s\n", addr)
-	err := http.ListenAndServe(addr, http.HandlerFunc(webServer))
+	err := http.ListenAndServe(addr, &Server{})
 	if err != nil {
 		panic(err)
 	}
